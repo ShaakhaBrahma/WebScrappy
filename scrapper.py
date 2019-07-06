28419e0b0887895d9c3dbf935262ecfec1d484ed
ShaakhaBrahma <vatsavatsi777@protonmail.com>
2019-07-07 01:09:09 +0530

    done_with_title

--- scrapper.py
+++ scrapper.py
@@ -5,6 +5,10 @@ req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 web_byte = urlopen(req).read()
 webpage = web_byte.decode('utf-8')
 scripts = re.findall('<script\stype=\'application/ld\+json\'.+>({.*?})</script>?', webpage)
+name = re.findall('"name":"(.*?)",?',scripts[0])
+
 print(scripts)
+print(name)
+
 
 
