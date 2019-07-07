ee48705e1f78ed33b2120e01912aaffac1a9baf8
ShaakhaBrahma <vatsavatsi777@protonmail.com>
2019-07-07 06:17:09 +0530

    first commit

--- scrapper.py
+++ scrapper.py
@@ -1,14 +1,15 @@
-import re
+import re, urllib
 from urllib.request import Request, urlopen
 url = 'https://hebbarskitchen.com/'
+start=
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+print(at.status)
 web_byte = urlopen(req).read()
 webpage = web_byte.decode('utf-8')
 scripts = re.findall('<script\stype=\'application/ld\+json\'.+>({.*?})</script>?', webpage)
 name = re.findall('"name":"(.*?)",?',scripts[0])
-
-print(scripts)
-print(name)
+print('Extracted structured data:',scripts)
+print('All titles in it:',name,at.info())
 
 
 
