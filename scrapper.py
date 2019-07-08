0265a7c1b13e1c428f7df95bd64e301dd56999da
ShaakhaBrahma <vatsavatsi777@protonmail.com>
2019-07-08 05:39:47 +0530

    fin

--- scrapper.py
+++ scrapper.py
@@ -1,15 +1,22 @@
-import re, urllib
+import re, urllib, datetime
+from datetime import datetime
 from urllib.request import Request, urlopen
 url = 'https://hebbarskitchen.com/'
-start=
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-print(at.status)
+try : req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+except urllib.error.URLError as e:
+    print(e.reason)
+start= datetime.now()
+stime = float((start.strftime('%S.%f')))
 web_byte = urlopen(req).read()
+end = datetime.now()
+etime = float((end.strftime('%S.%f')))
 webpage = web_byte.decode('utf-8')
 scripts = re.findall('<script\stype=\'application/ld\+json\'.+>({.*?})</script>?', webpage)
-name = re.findall('"name":"(.*?)",?',scripts[0])
-print('Extracted structured data:',scripts)
-print('All titles in it:',name,at.info())
+name = re.findall('"name":"(.*?)",?', scripts[0])
+print('Extracted structured data:', scripts)
+print('All titles:', name)
+print('response time:%f ms'% ((etime-stime)*1000))
+print('status code:200 ok')
 
 
 
